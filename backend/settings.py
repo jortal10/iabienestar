882efe5e7f6028b679fbad85bd5bdfb3cb7a94b7
jortal10 <j_ortal@hotmail.com>
2024-12-02 11:53:19 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ class _UiSettings(BaseSettings):
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
-    chat_description: str = "IA delk bienesatar"
+    chat_description: str = "IA del bienesatar"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
